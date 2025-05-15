a9848b50299502f8ba152ad5404aef8325442aa4
hebei5920 <hebei5920@gmail.com>
2025-05-15 20:36:13 +0800

    删除代理

--- vite.config.ts
+++ vite.config.ts
@@ -6,25 +6,24 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
   // 加载环境变量
   const env = loadEnv(mode, process.cwd(), '');
-  console.log("dddd+++++++++++++",env.VITE_TK);
 
   return {
     plugins: [react()],
     optimizeDeps: {
       exclude: ['lucide-react'],
     },
-    server: {
-      proxy: {
-        '/api/replicate': {
-          target: 'https://api.replicate.com/v1/predictions',
-          changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/api\/replicate/, ''),
-          headers: {
-            'Authorization': `Bearer ${env.VITE_TK}`
-          }
-        }
-      }
-    }
+    // server: {
+    //   proxy: {
+    //     '/api/replicate': {
+    //       target: 'https://api.replicate.com/v1/predictions',
+    //       changeOrigin: true,
+    //       rewrite: (path) => path.replace(/^\/api\/replicate/, ''),
+    //       headers: {
+    //         'Authorization': `Bearer ${env.VITE_TK}`
+    //       }
+    //     }
+    //   }
+    // }
   };
 });
 // export default defineConfig({
