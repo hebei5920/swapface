543505c93f8807e914d73a7ebd89fc93e15ac429
hebei5920 <hebei5920@gmail.com>
2025-05-15 20:36:45 +0800

    删除代理2

--- vite.config.ts
+++ vite.config.ts
@@ -1,46 +1,13 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 
 
-export default defineConfig(({ mode }) => {
-  // 加载环境变量
-  const env = loadEnv(mode, process.cwd(), '');
 
-  return {
-    plugins: [react()],
-    optimizeDeps: {
-      exclude: ['lucide-react'],
-    },
-    // server: {
-    //   proxy: {
-    //     '/api/replicate': {
-    //       target: 'https://api.replicate.com/v1/predictions',
-    //       changeOrigin: true,
-    //       rewrite: (path) => path.replace(/^\/api\/replicate/, ''),
-    //       headers: {
-    //         'Authorization': `Bearer ${env.VITE_TK}`
-    //       }
-    //     }
-    //   }
-    // }
-  };
+export default defineConfig({
+  plugins: [react()],
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
+
 });
-// export default defineConfig({
-//   plugins: [react()],
-//   optimizeDeps: {
-//     exclude: ['lucide-react'],
-//   },
-//   server: {
-//     proxy: {
-//       '/api/replicate': {
-//         target: 'https://api.replicate.com/v1/predictions',
-//         changeOrigin: true,
-//         rewrite: (path) => path.replace(/^\/api\/replicate/, ''),
-//         headers: {
-//           'Authorization': `Bearer ${process.env.VITE_TK}`
-//         }
-//       }
-//     }
-//   }
-// });
